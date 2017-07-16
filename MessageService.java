2f95513488d6ba018e8b1c60bc3fd93be1480f52
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2017-07-16 01:30:41 -0500

    Create MessageService.java
    Messenger REST API Tutorial - MessageService.java

---
+++ MessageService.java
@@ -0,0 +1,18 @@
+package org.jaxrs_tutorial.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jaxrs_tutorial.messenger.model.Message;
+
+public class MessageService {
+
+	public List<Message> getAllMessages() {
+		Message m1 = new Message(1L, "Hello World", "Steve");
+		Message m2 = new Message(2L, "Hello Galaxy", "Ed");
+		List<Message> list = new ArrayList<>();
+		list.add(m1);
+		list.add(m2);
+		return list;
+	}
+}
