2175f69e8fee0c077e646fdac903e6e953528f55
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2017-11-20 15:06:38 -0600

    Update DecryptFile.java

--- custom.ssl.tool/src/custom/ssl/tool/DecryptFile.java
+++ custom.ssl.tool/src/custom/ssl/tool/DecryptFile.java
@@ -23,7 +23,7 @@ public class DecryptFile {
 			bw = new BufferedWriter(fw);
 			String dec;
 			try {
-				dec = BCEncrypterDecrypter.decrypt(data, "opalou913");
+				dec = BCEncrypterDecrypter.decrypt(data, "xxx");
 				bw.write(dec+"\n");
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -55,7 +55,7 @@ public class DecryptFile {
         	while ((sCurrentLine = br.readLine()) != null) {
         		try {
         			//decrypt(sCurrentLine);
-        			System.out.println("\t"+BCEncrypterDecrypter.decrypt(sCurrentLine, "opalou913"));
+        			System.out.println("\t"+BCEncrypterDecrypter.decrypt(sCurrentLine, "xxx"));
         		} catch (Exception e) {
         			e.printStackTrace();
         		}
