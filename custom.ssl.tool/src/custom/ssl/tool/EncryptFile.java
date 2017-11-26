9318ff9c3c1b3d4e8153fd4c2e1c503403626dda
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2017-11-25 21:36:34 -0600

    Update EncryptFile.java

--- custom.ssl.tool/src/custom/ssl/tool/EncryptFile.java
+++ custom.ssl.tool/src/custom/ssl/tool/EncryptFile.java
@@ -23,7 +23,7 @@ public class EncryptFile {
 			bw = new BufferedWriter(fw);
 			String enc;
 			try {
-				enc = BCEncrypterDecrypter.encrypt(data, "opalou913");
+				enc = BCEncrypterDecrypter.encrypt(data, "xxxx");
 				bw.write(enc+"\n");
 			} catch (Exception e) {
 				e.printStackTrace();
