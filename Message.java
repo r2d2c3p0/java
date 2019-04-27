f08803dd676e6713f596fec26277942db9a3f206
Shane-Reddy <r2d2c3p0@users.noreply.github.com>
2019-04-26 20:34:18 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,20 +1,17 @@
 package org.jaxrs_tutorial.messenger.model;
 
 import java.util.Date;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Message {
 	
-	private long id;
+    private long id;
     private String message;
     private Date created;
     private String author;
     
-    public Message() {
-    	   	
-    }
+    public Message() { 	}
     
     public Message(long id, String message, String author) {
     	this.id = id;
